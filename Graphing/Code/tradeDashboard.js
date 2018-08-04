afc0f157ad82b3a02c0ebff046e8f7050b912d19
canthony224 <caleb_anthony@taylor.edu>
2018-08-04 18:39:56 -0400

    Just finally got candlesticks and dynamic chart creation working

--- Graphing/Code/tradeDashboard.js
+++ Graphing/Code/tradeDashboard.js
@@ -142,155 +142,48 @@ function setupChart1(data){
 	//console.log(_Chart1Times);
 	//_Chart1Times = (_Chart1Times);
 	//console.log(_Chart1Times);
-	var chart1  = document.getElementById('chart1').getContext('2d');
+	var chartCanvas  = document.getElementById('chart1').getContext('2d');
 	//var chart2  = document.getElementById('chart2').getContext('2d');
 	//var chart3  = document.getElementById('chart3').getContext('2d');
 	//var chart4  = document.getElementById('chart4').getContext('2d');
 
-	chart1.canvas.width = 1000;	
-	chart1.canvas.height = 400;	
+	chartCanvas.canvas.width = 1000;	
+	chartCanvas.canvas.height = 400;	
 
 	// Params: (data,timeline,chartFormat)
 	// data: array of values (so far h or l or o or c or ohlc{} or array of arrays of data. to support multi data graphs [[openData],[closeData]]
 	// timeline: array of times []
-	// ChartFormat: string of availible formats NOTE: does not support multi data graphs or array for multi type graphs ['line','bar']
-	var test = getChartConfig([closeData,openData],dataTimeline,["line",'olhc']);
-	console.log(" Got config", test);
-	var chart1Config = { //getChartConfig(closeData,dataTimeline,"line"){
-	// The type of chart we want to create
-	type: 'bar',
-	// The data for our dataset
-	data: {
-	 datasets: [{
-	    type: 'line',
-	    label: "Prices",
-	    backgroundColor: 'rgb(220, 219, 219)',
-	    borderColor: 'rgb(200, 199, 199)',
-	    fill: false,
-	    yAxisID: "closingPrices",
-	    data: closeData,
-	 },
-	 {
-	    type: 'bar',
-	    label: "opening prices",
-	    backgroundColor: 'rgb(120, 219, 219)',
-	    borderColor: 'rgb(230, 10, 199)',
-	    fill: false,
-	    yAxisID: "openingPrices",
-	    data: openData, //???G
-	 }],
-	 
- 	    labels: dataTimeline //get the date List, dateList in DB 		
-	},
-	// Configuration options go here
-	options: {
-		responsive: true,
-		title:{
-			display: true,
-			text: "data", //Classname goes here
-		},
-		layout: {
-			    padding: {
-				left: 0,
-				right: 0,
-				top: 0,
-				bottom: 0
-			    }
-			},
-		//events: [],
-		
-		/*tooltips: {
-			enabled: true,
-		},*/
-
-		scales: {
-			xAxes:[{
-				barThickness: 10,
-				type: "time",
-				time: {
-					//format: timeFormat,
-					min: _Chart1Times[0],
-					max: _Chart1Times[_Chart1Times.length-1],
-					tooltipFormat: _TimeFormat,
-				},
-				display: true,
-				scaleLabel: {
-					display: true,
-					labelString: 'Date'
-				},
-					ticks: {
-						maxRotation: 0,
-						//autoSkip: false,
-						autoSkipPadding: 2
-					}
-				
-			}],
-			yAxes: [{
-					id: "closingPrices",
-					position: 'left',
-					type: "linear",
-					scaleLabel: {	
-						display: true,
-						labelString: 'Price'
-					},
-					ticks: {
-						//max: Number(2),//Where secMax comes in
-						//min: Number(0)
-					}
-				},{
-					id: "openingPrices",
-					type: "linear",
-					display: false,
-					scaleLabel: {	
-						display: false,
-						labelString: 'Closing Bar'
-					}
-				}]	
-		},
-		zoom: {
-			enabled: true,
-			//drag: false,
-			mode: 'x',
-			limits: {
-				max: 30,
-				min: 0.5
-			}
-		},
-		pan: {
-			// Boolean to enable panning
-			enabled: true,
-			// Panning directions. Remove the appropriate direction to disable 
-			// Eg. 'y' would only allow panning in the y direction
-			mode: 'xy',
-			rangeMin: {
-				// Format of min pan range depends on scale type
-				x: null,
-				y: null
-			},
-			rangeMax: {
-				// Format of max pan range depends on scale type
-				x: null,
-				y: null
-			}
-		},
-		elements: {
-			line: {
-				tension:0,
-			}
-		}
-		}
-	};
-
+	// ChartFormat: list of strings of desired formats]
+	var chart1Config = getChartConfig([ohlcData],dataTimeline,["candlestick"]);
+	console.log(" Got config ", chart1Config);
 	window.chart1 = new Chart(chart1,chart1Config);
 	// Get custom places for updating individual charts
 	// (0) which dataste
 	// [5] which point index
-	var point = window.chart1.getDatasetMeta(0).data[5];
+	window.chart1.update();
+	//var point = window.chart1.getDatasetMeta(0).data[5];
 	//console.log(point);
-	point.custom = point.custom || {};
-	point.custom.borderColor = "green"
-	point.custom.borderWidth = 4;
-	point.custom.radius = 5;
+	//point.custom = point.custom || {};
+	//point.custom.borderColor = "green"
+	//point.custom.borderWidth = 4;
+	//point.custom.radius = 5;
+	//window.chart1.update();
+
+		//Set up custom Points
+
+		// Set up intervalss
+
+	window.chart1 = new Chart(chartCanvas,chart1Config);
+	window.chart1.update();
+	// Get custom places for updating individual charts
+	// (0) which datassIDt
+	// [5] which point index
+	//var point = window.chart1.getDatasetMeta(0).data[1];
+	//console.log(point);
+	//point.custom = point.custom || {};
+	//point.custom.borderColor = "green"
+	//point.custom.borderWidth = 4;
+	//point.custom.radius = 5;
 	window.chart1.update();
 
 	//Set up custom Points
@@ -305,43 +198,57 @@ function getChartConfig(data,dataTimeline,chartTypes){
 	//console.log("getting data for",data,chartTypes)
 	// Will contain the basic info needed in every chart, options will be added as data is parsed
 	var config = getConfigTemplate(chartTitle,dataTimeline);
-	console.log("gettingConfig",config);
+	//console.log("gettingConfig",config);
 	
 	// Parse the types and set config to types
 	// Set the initial type tobe whatever was first
 	config.type = chartTypes[0];
-	for (var i = 0; i < chartTypes.length; i++){
-		let type = chartTypes[i];
-		let dataSet = getDataSet(type,data);
+	if (config.type == "candlestick"){
+		config = {
+			type: 'candlestick',
+			data: {datasets:[ ]}
+		};
+		var dataSet = getDataSet(config.type,data[0]);
+		console.log("data=",dataSet);
 		config.data.datasets.push(dataSet);
-		
-		console.log("type", type);
-		
+	}else {
+		// Resets config to keep from confusion			
+		for (var i = 0; i < chartTypes.length; i++){
+			let type = chartTypes[i];
+			let dataSet = getDataSet(type,data[i]);
+			config.data.datasets.push(dataSet);
+			let yOptions = getYAxes(type,data[i]);
+			config.options.scales.yAxes.push(yOptions);
+		}
 	}
-	console.log("After dataSetup",config);
 	return config;
 }
 
 // Not sute how customizable this will be,. returns a proper dataset
-function getDataSet(type,data){
+function getDataSet(type,data){ // Eventually pass in cusom label names and/or IDs
 	var color = window.chart1Colors.shift();
-	console.log(data);
+	//console.log(data);
 	var dataset = {};
 	if (type == "line"){
+		dataset.type = 'line';
 		dataset.label= "line"; 
 		dataset.borderColor = color; 
+		dataset.backgroundColor = 'rgb(200,219,219)';
 		dataset.data = data;
 		dataset.fill = false;
+		dataset.yAxisID = "priceLine"; // iD param
 	} else if (type == "bar"){
+		dataset.type = 'bar';
 		dataset.label= "bar"; 
 		dataset.borderColor = color; 
 		dataset.data = data;
 		dataset.fill = false;
-	
+		dataset.yAxisID = type; // iD param
 	}else if (type == "candlestick") {
 		dataset.label= "candlestick"; 
 		dataset.data = data;
 	}else if (type == "pie"){
+		dataset.type = 'pie';
 		dataset.label= "pie"; 
 		dataset.data = data;
 		dataset.backgroundColor = window.pieColors; // list of colors
@@ -352,118 +259,45 @@ function getDataSet(type,data){
 }
 
 
-
+function getYAxes(type,data){
+	var axisOptions = {};
+	if (type == "line"){
+		axisOptions.id = "priceLine";
+		axisOptions.position = 'left'; // window.axispositions.shift()
+		axisOptions.type = 'linear'; // should always be linear unless pie or something
+		axisOptions.scaleLabel = { display: true,
+					  labelString: 'price' // maybe passed in as parametr/ID
+					};
+		axisOptions.ticks = {}
+	} else if (type == "bar"){
+		axisOptions.id = type;
+		axisOptions.position = 'left'; // window.axispositions.shift()
+		axisOptions.type = 'linear'; // should always be linear unless pie or something
+		axisOptions.scaleLabel = { display: true,
+					  labelString: 'price' // maybe passed in as parametr/ID
+					};
 	
+	}else if (type == "candlestick") {
+		axisOptions.id = type;
+		axisOptions.position = 'left'; // window.axispositions.shift()
+		axisOptions.type = 'linear'; // should always be linear unless pie or something
+		axisOptions.scaleLabel = { display: true,
+					  labelString: 'price' // maybe passed in as parametr/ID
+					};
 
-/*
-G
-var chartConfig  = {
-	type: 'bar',
-	 
- 	    labels: _Chart1Times //get the date List, dateList in DB 		
-	},
-	// Configuration options go here
-	options: {
-		responsive: true,
-		title:{
-			display: true,
-			text: "Closing Prices", //Classname goes here
-		},
-		layout: {
-			    padding: {
-				left: 0,
-				right: 0,
-				top: 0,
-				bottom: 0
-			    }
-			},
-		//events: [],
-		
-		/*tooltips: {
-			enabled: true,
-		},
-
-		scales: {
-			xAxes:[{
-				barThickness: 10,
-				type: "time",
-				time: {
-					//format: timeFormat,
-					min: _Chart1Times[0],
-					max: _Chart1Times[_Chart1Times.length-1],
-					tooltipFormat: _TimeFormat,
-				},
-				display: true,
-				scaleLabel: {
-					display: true,
-					labelString: 'Date'
-				},
-					ticks: {
-						maxRotation: 0,
-						//autoSkip: false,
-						autoSkipPadding: 2
-					}
-				
-			}],
-			yAxes: [{
-					id: "closingPrices",
-					position: 'left',
-					type: "linear",
-					scaleLabel: {	
-						display: true,
-						labelString: 'Price'
-					},
-					ticks: {
-						//max: Number(2),//Where secMax comes in
-						//min: Number(0)
-					}
-				},{
-					id: "closingBar",
-					type: "linear",
-					display: false,
-					scaleLabel: {	
-						display: false,
-						labelString: 'Closing Bar'
-					}
-				}]	
-		},
-		zoom: {
-			enabled: true,
-			//drag: false,
-			mode: 'x',
-			limits: {
-				max: 30,
-				min: 0.5
-			}
-		},
-		pan: {
-			// Boolean to enable panning
-			enabled: true,
-			// Panning directions. Remove the appropriate direction to disable 
-			// Eg. 'y' would only allow panning in the y direction
-			mode: 'xy',
-			rangeMin: {
-				// Format of min pan range depends on scale type
-				x: null,
-				y: null
-			},
-			rangeMax: {
-				// Format of max pan range depends on scale type
-				x: null,
-				y: null
-			}
-		},
-		elements: {
-			line: {
-				tension:0,
-			}
-		}
-		}
-
+	}else if (type == "pie"){
+		axisOptions.id = type;
+		axisOptions.position = 'left'; // window.axispositions.shift()
+		axisOptions.type = 'linear'; // havent tested...
+		axisOptions.scaleLabel = { display: true,
+					  labelString: 'price' // maybe passed in as parametr/ID
+					};
+	} else{
+		console.log("chart not supported yet");
+	}
+	return axisOptions;
 }
-return chartConfig;
-}
-*/
+
 
 function getConfigTemplate(chartTitle,dataTimeline) {
 	var template = { 
@@ -473,7 +307,6 @@ function getConfigTemplate(chartTitle,dataTimeline) {
 			labels: dataTimeline
 		},
 		options: {
-			resonsive: true,
 			title: {
 				display: true,
 				text: chartTitle
@@ -482,6 +315,7 @@ function getConfigTemplate(chartTitle,dataTimeline) {
 			// tooltips:,???
 			scales: {
 				xAxes:[{
+					barThickness: 10,
 					type: "time",
 					time: {
 						//format: timeFormat,
@@ -491,7 +325,7 @@ function getConfigTemplate(chartTitle,dataTimeline) {
 					},
 					display: true,
 					scaleLabel: {
-						display: false,
+						display: true,
 						labelString: 'Time'
 					},
 						ticks: {
@@ -499,7 +333,6 @@ function getConfigTemplate(chartTitle,dataTimeline) {
 							//autoSkip: false,
 							autoSkipPadding: 2
 						}
-					
 				}],
 				yAxes: []	
 			},
@@ -524,9 +357,13 @@ function getConfigTemplate(chartTitle,dataTimeline) {
 					y: null
 				}
 			},
+			elements: {
+				line: {
+					tension:0,
+				}
+			}
 		}
 	};
-	console.log("returning template",template);
 	return template;
 }
 
